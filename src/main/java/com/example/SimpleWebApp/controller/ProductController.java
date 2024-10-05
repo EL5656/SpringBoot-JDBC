658e73a3ba54f3f22f503e8e9132152e4c865234
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-10-05 20:26:18 +0800

    model, service, controller

---
+++ src/main/java/com/example/SimpleWebApp/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.SimpleWebApp.controller;
+
+import com.example.SimpleWebApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    ProductService service;
+
+    ProductController(ProductService service){
+        this.service = service;
+    }
+
+    @RequestMapping("/products")
+    public List getProducts(){
+        return service.getProducts();
+    }
+}
---
+++ src/main/java/com/example/SimpleWebApp/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.SimpleWebApp.service;
+
+import com.example.SimpleWebApp.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    // Dummy product list
+     List<Product> products;
+
+    ProductService(){
+        products = new ArrayList<>();
+        products.add(new Product(1, "Laptop", 3999.99));
+        products.add(new Product(2, "Desktop", 1879.99));
+        products.add(new Product(3, "Tablet", 2649.99));
+    }
+
+    // Method to return the list of products
+    public List<Product> getProducts() {
+        return products; // Return the product list instead of null
+    }
+}
